f773e6d7bef605914a6d57ff5a99d83b93d964b8
Adriel Oliveira <ead.programador@toledoprudente.edu.br>
2017-07-15 17:44:15 -0300

    add all files

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content=""/>
+        <meta name="author" content="DEVED"/>
+        <meta name="keywords" content=""/>
+        <meta name="robot" content="Index,Follow"/>
+        
+        <title>Me ajuda ai</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="css/site.css" rel="stylesheet">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body>
+        
+        <section id="section1" class="section vertical-center">
+            <div class="container">
+                <div class="row text-center">
+                    <h5 class="title1"><img src="img/instagram.png" width="28px" class=""/></h5>
+                    <h5 class="title2">Produtos Farmacêuticos e Hospitalares</h5>
+                </div>
+            </div>
+        </section>
+
+        <section id="section2" class="section vertical-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-xs-12 col-centered">
+                        <h5></h5>
+                        <p class="textsec top15">Dictumst in massa habitasse tincidunt elit mus elit dictumst enim. Vel purus sagittis elementum lacus sit pellentesque eu, aliquet risus natoque, vut? Turpis vel. Ridiculus ridiculus amet amet et ac, dapibus dapibus magna.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="contato" class="section vertical-center">
+            <div class="container-fluid">
+                <div class="row">
+
+                </div>
+            </div>
+        </section>
+
+        <section id="section4" class="section vertical-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 col-centered">
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="section5" class="section">
+            <div class="container-fluid">
+                <div class="row">
+
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
